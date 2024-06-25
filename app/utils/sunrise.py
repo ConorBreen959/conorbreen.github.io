6ad7a910a79973f54c067c51af121564b2d9ad18
conor <conorbreen96@gmail.com>
2024-06-25 10:40:30 +0100

    Working further on sunrise

--- app/utils/sunrise.py
+++ app/utils/sunrise.py
@@ -3,7 +3,14 @@ from datetime import datetime, timedelta
 from skyfield import almanac
 from skyfield.api import N, W, wgs84, load
 import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.io as pio
+import seaborn as sb
+from matplotlib import pyplot as plt
 
+
+pio.renderers.default = "browser"
 
 year = 2023
 days = 365
@@ -11,34 +18,76 @@ if year % 4:
     days = 366
 
 tz = timezone("Europe/Dublin")
-now = tz.localize(datetime(2024, 1, 1))
-midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
-next_midnight = midnight + timedelta(days=366)
-
 ts = load.timescale()
-t0 = ts.from_datetime(midnight)
-t1 = ts.from_datetime(next_midnight)
+now = tz.localize(datetime(2024, 1, 1))
 eph = load('de421.bsp')
-bluffton = wgs84.latlon(40.8939 * N, 83.8917 * W)
-f = almanac.dark_twilight_day(eph, bluffton)
+location = wgs84.latlon(53.3498 * N, 6.2603* W)
+f = almanac.dark_twilight_day(eph, location)
+
+
+first_midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+next_midnight = first_midnight + timedelta(days=days)
+t0 = ts.from_datetime(first_midnight)
+t1 = ts.from_datetime(next_midnight)
 times, events = almanac.find_discrete(t0, t1, f)
 
-sunrise_df = pd.DataFrame(columns=["Date", "Time", "Type"])
+sunrise_df = pd.DataFrame(columns=["Date", "Event", "Starts", "Ends"])
 
-previous_e = f(t0).item()
-for t, e in zip(times, events):
+def format_time(t):
     time_string = str(t.astimezone(tz))[:16]
-    date, time = time_string.split(" ")
-    twilight = almanac.TWILIGHTS[e]
-    if previous_e < e:
-        twilight_type = f"{twilight} starts"
+    date_x, time_x = time_string.split(" ")
+    date_object = datetime.strptime(date_x, "%Y-%m-%d").date()
+    time_object = datetime.strptime(time_x, "%H:%M")
+    time_in_seconds = timedelta(hours=time_object.hour, minutes=time_object.minute).seconds
+    return date_object, time_object
+
+
+for index, (t, e) in enumerate(zip(times, events)):
+    event = almanac.TWILIGHTS[e]
+    start_date, start_time = format_time(t)
+    if index == len(times) - 1:
+        end_date, end_time = format_time(t1)
     else:
-        twilight_type = f"{twilight} ends"
-    print(f"{time_string} {twilight_type}")
-    previous_e = e
-    df = pd.DataFrame(data={"Date": date, "Time": datetime.strptime(f"{time}", "%H:%M"), "Type": twilight_type}, index=[0])
-    sunrise_df = pd.concat([sunrise_df, df])
+        end_date, end_time = format_time(times[index + 1])
+    event = event.replace("twilight", "dawn")
+    if start_time.hour > 12:
+        event = event.replace("dawn", "dusk")
+    if end_date > start_date:
+        first_df = pd.DataFrame(data={"Date": start_date, "Event": event, "Starts": start_time, "Ends": np.nan}, index=[0])
+        next_df = pd.DataFrame(data={"Date": end_date, "Event": event, "Starts": np.nan, "Ends": end_time}, index=[0])
+        sunrise_df = pd.concat([sunrise_df, first_df, next_df])
+    else:
+        df = pd.DataFrame(data={"Date": start_date, "Event": event, "Starts": start_time, "Ends": end_time}, index=[0])
+        sunrise_df = pd.concat([sunrise_df, df])
 
 
-fig = px.line(sunrise_df, x="Date", y="Time", color="Type")
+sunrise_df = sunrise_df.reset_index(drop=True)
+
+
+event_types = sunrise_df["Event"].drop_duplicates().tolist()
+event_dfs = [sunrise_df[sunrise_df["Event"] == event_type] for event_type in event_types if event_type != "Night"]
+
+
+fig = go.Figure()
+
+for index, event_df in enumerate(event_dfs):
+    name = event_df["Event"].tolist()[0]
+    fill = "tonexty"
+    if index == 0:
+        fill = None
+    fig.add_trace(go.Scatter(x=event_df["Date"], y=event_df["Starts"].replace(pd.NaT, np.nan), name=f"{name} starts", hovertemplate="%{y|%H:%M}"))
+    fig.add_trace(go.Scatter(x=event_df["Date"], y=event_df["Ends"].replace(pd.NaT, np.nan), name=f"{name} ends", hovertemplate="%{y|%H:%M}<br>%{x}>"))
 fig.show()
+
+
+plt.figure(figsize=(50, 50))
+sb.relplot(
+    data=sunrise_df,
+    kind="line",
+    x="Date",
+    y="Starts",
+    hue="Event"
+)
+
+penguins = sb.load_dataset("penguins")
+sb.jointplot(data=penguins, x="flipper_length_mm", y="bill_length_mm", hue="species")
